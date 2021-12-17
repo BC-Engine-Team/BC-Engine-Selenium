f5b315c48726c74ec341f8abc504518253090e6b
Nicolas Espitalier <nespitalier014@gmail.com>
2021-12-17 11:40:09 -0500

    Two selenium tests implemented for delete

--- tests/test.users.js
+++ tests/test.users.js
@@ -1,4 +1,4 @@
-const { By, Builder } = require("selenium-webdriver");
+const { By, Builder, until } = require("selenium-webdriver");
 require("chromedriver");
 const assert = require("assert");
 const { describe, beforeEach, afterEach, it } = require("mocha");
@@ -46,6 +46,50 @@ describe("S2 - Users", () => {
             assert.equal(present, false);
 
         });
+
+        it("S2.1.3- Successfully deleted a user as an admin", async() => {
+
+            await login(driver, "admin");
+
+            await driver.findElement(By.linkText("Users")).click();
+        
+            let buttonDelete = await driver.findElements(By.className("btnDelete btn-delete"));
+            
+            await driver.sleep(3000);
+
+            buttonDelete[0].click();
+
+            await driver.sleep(3000);
+
+            let confirmDelete = await driver.findElement(By.className("deleteUserButton"));
+
+            confirmDelete.click();
+
+            await driver.sleep(4000);
+        });
+
+
+        it("S2.1.4- Refused to delete a user as an admin", async() => {
+
+            await login(driver, "admin");
+
+            await driver.findElement(By.linkText("Users")).click();
+        
+            let buttonDelete = await driver.findElements(By.className("btnDelete btn-delete"));
+            
+            await driver.sleep(3000);
+
+            buttonDelete[0].click();
+
+            await driver.sleep(3000);
+
+            let confirmRefuse = await driver.findElement(By.className("cancelDeleteUserButton"));
+
+            confirmRefuse.click();
+
+            await driver.sleep(4000);
+        });
+
 
         afterEach(async () => {
             await driver.quit()
