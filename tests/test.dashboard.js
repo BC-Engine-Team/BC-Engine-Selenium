e50a33bb6a8dd25e5f9022ee741d4e90ba13ecd0
Nicolas Espitalier <89820626+NickEspitalier@users.noreply.github.com>
2022-01-28 18:40:51 -0500

    Beginning tests

--- tests/test.dashboard.js
+++ tests/test.dashboard.js
@@ -46,4 +46,47 @@ describe("S3 - Dashboard", () => {
             await eyes.abortIfNotClosed();
         });
     });
+
+    
+
+    describe("S3.2 - User logs in and sees Dashboard Table", () => {
+        let driver, runner, eyes;
+        let url = "http://localhost:3000";
+
+        beforeEach(() => {
+            // Initialize the Runner for your test.
+            runner = new ClassicRunner();
+
+            // Initialize the eyes SDK 
+            eyes = new Eyes(runner);
+            eyes.setApiKey("lJfCRLJ1k3w108xjmryokB102yHOMxfxZ197RsnV9x2bYyiA110");
+
+            driver = new Builder().forBrowser("chrome").build();
+        });
+
+        it("S3.2.1 - Successfully shows data on Table", async () => {
+            await eyes.open(driver, "B&C Engine", "check default overview table");
+            await driver.get(url);
+            await login(driver);
+
+            // verify that the chart display
+            let chartCanvas = await driver.findElement(By.id("chart"));
+
+            await driver.sleep(3000);
+
+            await eyes.check("Chart Canvas", chartCanvas);
+            await eyes.closeAsync();
+
+            // verify that the input box for chart report name works
+            let chartNameInput = await driver.findElement(By.id("chartName"));
+            await driver.wait(until.elementIsEnabled(chartNameInput), 2000);
+            await chartNameInput.sendKeys("Chart Name", Key.TAB);
+            assert.equal(await chartNameInput.getAttribute("value"), "Chart Name");
+        });
+
+        afterEach(async () => {
+            await driver.quit();
+            await eyes.abortIfNotClosed();
+        });
+    });
 });
